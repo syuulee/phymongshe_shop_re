eea1038ddaa09511a05bf22fb1e2ad68af0f95fc
syuulee <yuyu00lee@gmail.com>
2022-09-30 16:56:22 +0900

    2022.09.30

--- src/component/MainProductSlide.js
+++ src/component/MainProductSlide.js
@@ -2,11 +2,15 @@ import React from 'react'
 import CategorySlider from './CategorySlider';
 import MainCategory from './MainCategory'
 
-const MainProuctSlide = ({ shopList, category }) => {
+const MainProuctSlide = ({ shopList, category, tit, des, bg }) => {
     return (
-        <div>
-            <CategorySlider shopList={shopList} category={category} />
-        </div>
+        <section className="sce slide_" style={{ background: bg }}>
+            <h2>BEST PRODUCT</h2>
+            <p>피몽쉐 마인드풀 라운지 BEST PRODUCT</p>
+            <div className="inner">
+                <CategorySlider shopList={shopList} category={category} arrow={true} dots={true} />
+            </div>
+        </section>
     )
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,9 +9,9 @@ const Main = ({ shopList }) => {
     return (
         <>
             <MainSlide />
-            <CategorySlider shopList={shopList} category={'liquid'} />
+            <CategorySlider shopList={shopList} category={'liquid'} tit={'BEST PRODUCT'} des={'피몽쉐 베스트라인 어쩌고 저쩌고'} />
             <MainTab shopList={shopList} />
-            <MainCategory shopList={shopList} category={'cream'} />
+            <MainCategory shopList={shopList} category={'cream'} tit={'BEST PRODUCT'} des={'피몽쉐 베스트라인 어쩌고 저쩌고'} bg={'#f7f7f7'} />
             <MainCategory shopList={shopList} category={'gel'} />
             <MainCategory shopList={shopList} category={'pencil'} />
             <MainCategory shopList={shopList} category={'liquid'} />
